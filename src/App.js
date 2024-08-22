98078b70a2730e9983cbc6f75c5eea4ffded788d
Harikrishnan-K <krishnakripaharikrishnan@gmail.com>
2024-08-22 16:53:09 +0530

    switch_theme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const [dark, setDark] = useState(false);
+
+  const changeTheme = () => {
+    setDark(!dark);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={dark ? 'dark' : 'light'}>
+      <h1>Switch Theme</h1>
+      <button className="button" onClick={changeTheme}>
+        <b>Switch Theme</b>
+      </button>
+
+
+    
     </div>
   );
 }
